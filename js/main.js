9e8d0875967541d4bdae26f2ec6c34878207e727
mrus <mrus.corporation@gmail.com>
2022-09-15 14:51:51 -0700

    last changes in js file

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 let links = [
     {
-        label: "Activity week1",
-        url: "https://marcosamos.github.io/wdd330/week1/story_editor.html"
+        label: "Week1",
+        url: "https://marcosamos.github.io/wdd330/week1/index.html"
     }
 ];
 
-
-let list = document.getElementById("myList"); //get the ol element 
+let list = document.getElementById("myList"); //get the ul element 
 
 links.forEach(item => {
-    let title = document.createElement("li");
     let li = document.createElement("li");
-    title.innerText = item.label;
-    li.innerText = item.url;
-    list.appendChild(title);
     list.appendChild(li);
+    let title = document.createElement("a");
+    title.setAttribute("href",item.url);
+    let aTexto = document.createTextNode(item.label);
+    li.appendChild(aTexto);
+    li.appendChild(title);
 });
